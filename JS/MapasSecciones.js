456568d886d8f1c8ecdee3ae359560fb24ef3cac
MarcosGit1303 <marcosgithub1303@gmail.com>
2024-04-16 13:29:06 +0200

    Mas cosas

--- JS/MapasSecciones.js
+++ JS/MapasSecciones.js
@@ -12,6 +12,7 @@ switch (Mapa) {
   case 'Ariende':
     var MapaPrincipal = 'imagenes/Mapas/Ariende.jpg';
     var MapaSecundario = 'imagenes/Mapas/AriendeZoom.png';
+    var MapaNombres = 'imagenes/Mapas/AriendeZoomNombres.png';
     break;
   default:
     var MapaPrincipal = 'imagenes/Mapas/Reinos.png';
@@ -33,7 +34,7 @@ var viewer = OpenSeadragon({
     },
     {
       type: 'image',
-      url: '../imagenes/Mapas/AriendeZoomNombres.png',
+      url: '../' + MapaNombres,
     }
   ],
   minZoomLevel: 1,
@@ -43,52 +44,41 @@ var viewer = OpenSeadragon({
 
 });
 
-// Obtén el contenedor por su id
 var miContenedor = document.getElementById("openseadragon");
-
-// Variable para almacenar el identificador del intervalo
 var intervalo;
 
-// Agrega un event listener para el evento mouseover
+//Zoom imagenes Basicas
 miContenedor.addEventListener("mouseover", function () {
-  // Inicia la acción deseada utilizando setInterval
   intervalo = setInterval(function () {
     var currentZoom = viewer.viewport.getZoom();
-
-    // Obtener referencias a las imágenes cargadas
     var lowResolutionImage = viewer.world.getItemAt(0);
     var highResolutionImage = viewer.world.getItemAt(1);
 
-    // Cambiar la visibilidad de las imágenes según el nivel de zoom
     highResolutionImage.setOpacity(currentZoom >= 0 && currentZoom < 2 ? 1 : 0);
     lowResolutionImage.setOpacity(currentZoom >= 2 && currentZoom <= 6 ? 1 : 0);
   }, 200);
 });
 
-// Agrega un event listener para el evento mouseout
-miContenedor.addEventListener("mouseout", function () {
-  // Detiene la acción cuando el ratón sale del contenedor utilizando clearInterval
-  clearInterval(intervalo);
-});
-
-// Obtén una referencia al botón por su id
+//
 var botonNombre = document.getElementById("botonNombre");
-var intervalo;
-// Llamar a la función para controlar la visibilidad dentro de un intervalo que se ejecuta continuamente
-intervalo = setInterval(controlarVisibilidad, 50);
+intervalo = setInterval(visibilidadNombres, 50);
 
-// Agrega un event listener para el evento 'click' en el botón
+//Boton Nombre activar desactivar
 botonNombre.addEventListener("click", function () {
-  // Cambia el estado del botón (mostrar/ocultar)
   botonNombre.clicked = !botonNombre.clicked;
+  if (botonNombre.clicked) {
+    botonNombre.classList.remove("desactivado");
+    botonNombre.classList.add("activado");
+  } else {
+    botonNombre.classList.remove("activado");
+    botonNombre.classList.add("desactivado");
+  }
 });
 
 // Función para controlar la visibilidad de la imagen adicional
-function controlarVisibilidad() {
+function visibilidadNombres() {
   var currentZoom = viewer.viewport.getZoom();
   var imagenNombres = viewer.world.getItemAt(2);
-
-  // Verificar ambas condiciones y cambiar la opacidad
   if (currentZoom >= 2 && currentZoom <= 6 && botonNombre.clicked) {
     imagenNombres.setOpacity(1);
   } else {
